4e63e10c8e06d4d391d52e3cd5a4c69f5ad38163
scoace <scoace@web.de>
2014-02-17 14:59:16 +0100

    fix for the README file

---
+++ queues.py
@@ -0,0 +1,22 @@
+__author__ = 'andy'
+
+
+from collections import deque
+
+d=deque("ghi")
+
+def printqueue(queue):
+    for elem in queue:
+      print(elem)
+
+d.append('x')
+
+printqueue(d)
+
+d.pop()
+
+printqueue(d)
+print(d.maxlen)
+d.clear()
+
+printqueue(d)
